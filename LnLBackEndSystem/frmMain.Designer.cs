98fcdc7157369ad9c714913eaa7ddd7c513bf548
Manlx <74509303+Manlx@users.noreply.github.com>
2021-09-10 15:30:19 +0200

    Fixed All I hOpe

--- LnLBackEndSystem/frmMain.Designer.cs
+++ LnLBackEndSystem/frmMain.Designer.cs
@@ -33,13 +33,17 @@ namespace LnLBackEndSystem
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(143, 359);
+            this.button1.Location = new System.Drawing.Point(0, 624);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(99, 61);
             this.button1.TabIndex = 0;
@@ -49,7 +53,7 @@ namespace LnLBackEndSystem
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(440, 234);
+            this.button2.Location = new System.Drawing.Point(105, 624);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(99, 61);
             this.button2.TabIndex = 1;
@@ -59,41 +63,67 @@ namespace LnLBackEndSystem
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(421, 60);
+            this.button3.Location = new System.Drawing.Point(210, 636);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(118, 49);
             this.button3.TabIndex = 2;
-            this.button3.Text = "Secretary";
+            this.button3.Text = "button3";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
-            // button4
+            // textBox1
             // 
-            this.button4.Location = new System.Drawing.Point(625, 109);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(126, 67);
-            this.button4.TabIndex = 3;
-            this.button4.Text = "Guest";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
+            this.textBox1.Location = new System.Drawing.Point(536, 279);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(182, 35);
+            this.textBox1.TabIndex = 3;
             // 
-            // button5
+            // pictureBox1
             // 
-            this.button5.Location = new System.Drawing.Point(268, 109);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(125, 60);
-            this.button5.TabIndex = 4;
-            this.button5.Text = "Admin";
-            this.button5.UseVisualStyleBackColor = true;
-            this.button5.Click += new System.EventHandler(this.button5_Click);
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1178, 207);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(400, 279);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 30);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Username:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(536, 338);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(182, 35);
+            this.textBox2.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(400, 338);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 30);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Password:";
             // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 30F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(978, 528);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
+            this.ClientSize = new System.Drawing.Size(1178, 681);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -105,7 +135,9 @@ namespace LnLBackEndSystem
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Main";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.frmDataBaseMaintenance_FormClosed);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -114,7 +146,10 @@ namespace LnLBackEndSystem
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
     }
 }
--- README.md
+++
@@ -1,3 +0,0 @@
-# LnLSystem
-Projected For CMPG 223
-This is our CMPG 223 Second year Project
