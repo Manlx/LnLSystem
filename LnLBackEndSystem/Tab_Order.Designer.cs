8f8d49c4a98a24f5b8dd3d8b19abc33a7e6001fa
BrendanV11 <brendan.trexftw@gmail.com>
2021-09-12 09:43:24 +0200

    ok

--- LnLBackEndSystem/Tab_Order.Designer.cs
+++ LnLBackEndSystem/Tab_Order.Designer.cs
@@ -149,7 +149,7 @@ namespace LnLBackEndSystem
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(150, 9);
+            this.label7.Location = new System.Drawing.Point(126, 9);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(45, 13);
             this.label7.TabIndex = 12;
