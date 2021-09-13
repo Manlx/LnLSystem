d994b5277f99f2bc58caa4c56e545d222fb5df73
TolmayM <88318635+TolmayM@users.noreply.github.com>
2021-09-13 10:49:43 +0200

    Added guest order form

---
+++ LnLBackEndSystem/Guest_order.Designer.cs
@@ -0,0 +1,390 @@
+﻿
+namespace LnLBackEndSystem
+{
+    partial class Guest_order
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Guest_order));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.lblHeading = new System.Windows.Forms.Label();
+            this.chkBud = new System.Windows.Forms.CheckBox();
+            this.chkCrystal = new System.Windows.Forms.CheckBox();
+            this.chkLiqui = new System.Windows.Forms.CheckBox();
+            this.chkThirsti = new System.Windows.Forms.CheckBox();
+            this.chkPepsi = new System.Windows.Forms.CheckBox();
+            this.btnPlaceOrder = new System.Windows.Forms.Button();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.cbBud = new System.Windows.Forms.ComboBox();
+            this.cbCrystal = new System.Windows.Forms.ComboBox();
+            this.cbLiqui = new System.Windows.Forms.ComboBox();
+            this.cbThirsti = new System.Windows.Forms.ComboBox();
+            this.cbPepsi = new System.Windows.Forms.ComboBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(108, 62);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(114, 118);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.InitialImage = null;
+            this.pictureBox2.Location = new System.Drawing.Point(253, 62);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(127, 118);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(395, 62);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(167, 118);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(579, 62);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(151, 118);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
+            this.pictureBox5.Location = new System.Drawing.Point(108, 232);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(114, 118);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox5.TabIndex = 4;
+            this.pictureBox5.TabStop = false;
+            // 
+            // lblHeading
+            // 
+            this.lblHeading.AutoSize = true;
+            this.lblHeading.Location = new System.Drawing.Point(12, 18);
+            this.lblHeading.Name = "lblHeading";
+            this.lblHeading.Size = new System.Drawing.Size(210, 13);
+            this.lblHeading.TabIndex = 5;
+            this.lblHeading.Text = "Select the product(s) you would like to buy:";
+            // 
+            // chkBud
+            // 
+            this.chkBud.AutoSize = true;
+            this.chkBud.Location = new System.Drawing.Point(108, 186);
+            this.chkBud.Name = "chkBud";
+            this.chkBud.Size = new System.Drawing.Size(71, 17);
+            this.chkBud.TabIndex = 6;
+            this.chkBud.Text = "Bud Light";
+            this.chkBud.UseVisualStyleBackColor = true;
+            // 
+            // chkCrystal
+            // 
+            this.chkCrystal.AutoSize = true;
+            this.chkCrystal.Location = new System.Drawing.Point(253, 186);
+            this.chkCrystal.Name = "chkCrystal";
+            this.chkCrystal.Size = new System.Drawing.Size(89, 17);
+            this.chkCrystal.TabIndex = 7;
+            this.chkCrystal.Text = "Crystal Water";
+            this.chkCrystal.UseVisualStyleBackColor = true;
+            // 
+            // chkLiqui
+            // 
+            this.chkLiqui.AutoSize = true;
+            this.chkLiqui.Location = new System.Drawing.Point(395, 186);
+            this.chkLiqui.Name = "chkLiqui";
+            this.chkLiqui.Size = new System.Drawing.Size(140, 17);
+            this.chkLiqui.TabIndex = 8;
+            this.chkLiqui.Text = "Liqui Fruit Orange 250ml";
+            this.chkLiqui.UseVisualStyleBackColor = true;
+            this.chkLiqui.CheckedChanged += new System.EventHandler(this.chkLiqui_CheckedChanged);
+            // 
+            // chkThirsti
+            // 
+            this.chkThirsti.AutoSize = true;
+            this.chkThirsti.Location = new System.Drawing.Point(579, 186);
+            this.chkThirsti.Name = "chkThirsti";
+            this.chkThirsti.Size = new System.Drawing.Size(117, 17);
+            this.chkThirsti.TabIndex = 9;
+            this.chkThirsti.Text = "Thirsti Water 330ml";
+            this.chkThirsti.UseVisualStyleBackColor = true;
+            this.chkThirsti.CheckedChanged += new System.EventHandler(this.chkThirsti_CheckedChanged);
+            // 
+            // chkPepsi
+            // 
+            this.chkPepsi.AutoSize = true;
+            this.chkPepsi.Location = new System.Drawing.Point(108, 356);
+            this.chkPepsi.Name = "chkPepsi";
+            this.chkPepsi.Size = new System.Drawing.Size(153, 17);
+            this.chkPepsi.TabIndex = 10;
+            this.chkPepsi.Text = "Pepsi Regular Bottle 330ml";
+            this.chkPepsi.UseVisualStyleBackColor = true;
+            this.chkPepsi.CheckedChanged += new System.EventHandler(this.chkPepsi_CheckedChanged);
+            // 
+            // btnPlaceOrder
+            // 
+            this.btnPlaceOrder.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPlaceOrder.Location = new System.Drawing.Point(434, 407);
+            this.btnPlaceOrder.Name = "btnPlaceOrder";
+            this.btnPlaceOrder.Size = new System.Drawing.Size(112, 44);
+            this.btnPlaceOrder.TabIndex = 11;
+            this.btnPlaceOrder.Text = "Place order";
+            this.btnPlaceOrder.UseVisualStyleBackColor = true;
+            this.btnPlaceOrder.Click += new System.EventHandler(this.btnPlaceOrder_Click);
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(431, 356);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(42, 13);
+            this.lblTotal.TabIndex = 12;
+            this.lblTotal.Text = "TOTAL";
+            this.lblTotal.Click += new System.EventHandler(this.lblTotal_Click);
+            // 
+            // cbBud
+            // 
+            this.cbBud.FormattingEnabled = true;
+            this.cbBud.Location = new System.Drawing.Point(188, 184);
+            this.cbBud.Name = "cbBud";
+            this.cbBud.Size = new System.Drawing.Size(34, 21);
+            this.cbBud.TabIndex = 13;
+            // 
+            // cbCrystal
+            // 
+            this.cbCrystal.FormattingEnabled = true;
+            this.cbCrystal.Location = new System.Drawing.Point(348, 186);
+            this.cbCrystal.Name = "cbCrystal";
+            this.cbCrystal.Size = new System.Drawing.Size(32, 21);
+            this.cbCrystal.TabIndex = 15;
+            // 
+            // cbLiqui
+            // 
+            this.cbLiqui.FormattingEnabled = true;
+            this.cbLiqui.Location = new System.Drawing.Point(533, 184);
+            this.cbLiqui.Name = "cbLiqui";
+            this.cbLiqui.Size = new System.Drawing.Size(29, 21);
+            this.cbLiqui.TabIndex = 16;
+            // 
+            // cbThirsti
+            // 
+            this.cbThirsti.FormattingEnabled = true;
+            this.cbThirsti.Location = new System.Drawing.Point(699, 184);
+            this.cbThirsti.Name = "cbThirsti";
+            this.cbThirsti.Size = new System.Drawing.Size(31, 21);
+            this.cbThirsti.TabIndex = 17;
+            this.cbThirsti.SelectedIndexChanged += new System.EventHandler(this.cbThirsti_SelectedIndexChanged);
+            // 
+            // cbPepsi
+            // 
+            this.cbPepsi.FormattingEnabled = true;
+            this.cbPepsi.Location = new System.Drawing.Point(266, 354);
+            this.cbPepsi.Name = "cbPepsi";
+            this.cbPepsi.Size = new System.Drawing.Size(35, 21);
+            this.cbPepsi.TabIndex = 18;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(579, 209);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(117, 17);
+            this.checkBox1.TabIndex = 19;
+            this.checkBox1.Text = "Thirsti Water 500ml";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(108, 383);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(136, 17);
+            this.checkBox2.TabIndex = 20;
+            this.checkBox2.Text = "Pepsi Max Bottle 330ml";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(108, 407);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(128, 17);
+            this.checkBox3.TabIndex = 21;
+            this.checkBox3.Text = "Pepsi Max Can 330ml";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(108, 434);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(136, 17);
+            this.checkBox4.TabIndex = 22;
+            this.checkBox4.Text = "Pepsi Max Bottle 600ml";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(699, 207);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(31, 21);
+            this.comboBox1.TabIndex = 23;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(266, 381);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(35, 21);
+            this.comboBox2.TabIndex = 24;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(266, 405);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(35, 21);
+            this.comboBox3.TabIndex = 25;
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Location = new System.Drawing.Point(266, 432);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(35, 21);
+            this.comboBox4.TabIndex = 26;
+            this.comboBox4.SelectedIndexChanged += new System.EventHandler(this.comboBox4_SelectedIndexChanged);
+            // 
+            // Guest_order
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(862, 616);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.cbPepsi);
+            this.Controls.Add(this.cbThirsti);
+            this.Controls.Add(this.cbLiqui);
+            this.Controls.Add(this.cbCrystal);
+            this.Controls.Add(this.cbBud);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.btnPlaceOrder);
+            this.Controls.Add(this.chkPepsi);
+            this.Controls.Add(this.chkThirsti);
+            this.Controls.Add(this.chkLiqui);
+            this.Controls.Add(this.chkCrystal);
+            this.Controls.Add(this.chkBud);
+            this.Controls.Add(this.lblHeading);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Guest_order";
+            this.Text = "Guest_order";
+            this.Load += new System.EventHandler(this.Guest_order_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.Label lblHeading;
+        private System.Windows.Forms.CheckBox chkBud;
+        private System.Windows.Forms.CheckBox chkCrystal;
+        private System.Windows.Forms.CheckBox chkLiqui;
+        private System.Windows.Forms.CheckBox chkThirsti;
+        private System.Windows.Forms.CheckBox chkPepsi;
+        private System.Windows.Forms.Button btnPlaceOrder;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox cbBud;
+        private System.Windows.Forms.ComboBox cbCrystal;
+        private System.Windows.Forms.ComboBox cbLiqui;
+        private System.Windows.Forms.ComboBox cbThirsti;
+        private System.Windows.Forms.ComboBox cbPepsi;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox4;
+    }
+}
